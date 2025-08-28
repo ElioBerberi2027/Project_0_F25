1e74b1007013f5fa0d89199c181053135a51f034
Elio Berberi <tuo92727@temple.edu>
2025-08-28 18:02:50 -0400

    Solution

--- random.c
+++ random.c
@@ -9,12 +9,16 @@ run a for loop that runs the same time as size so that we get enough letters
 for loop generates a new number each time that is in the range of 0 and 25 and the nr will correspond to the index of the letters
 join all the letters together and return them*/
 char *rand_string(char *s, int size){
-    
-    char letters[] = "abcdefghijklmnopqrstuvwqyz";
+    int index; // declaring index
 
-    for (size_t i = 0; i < size; i++){
+    char letters[] = "abcdefghijklmnopqrstuvwxyz"; //list
 
+    for (size_t i = 0; i < size; ++i){
+        index = (rand() % 26); // get the index of the new letter
+        s[i] = letters[index]; // at this given index i we set a new letter value
     }
 
-    return NULL;
+    s[size] = '\0'; // set the last index as the null terminator
+    
+    return s;
 }
